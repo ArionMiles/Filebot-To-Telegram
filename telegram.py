64e25adbb57c03b8626229db955750984ff23a6b
Kanishk <Kanishk>
2017-03-13 12:11:00 +0530

    Improved error reporting.

--- telegram.py
+++ telegram.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import json
 if len(sys.argv) <3:
 	print("Usage: telegram.py \"YOUR_MESSAGE\" \" Second_line \"")
 else:
@@ -7,7 +8,7 @@ else:
  
 # Your API key & CHAT_ID here
 API_KEY_TOKEN = "317819012:AAHE55sERM1t7bPPvUq8Y9HRU3Rm0T3omX0"
-CHAT_ID = "YOUR_CHAT_ID_HERE"
+CHAT_ID = "317392351"
 
 # defining the api-endpoint 
 API_ENDPOINT = "https://api.telegram.org/bot" + API_KEY_TOKEN + "/sendMessage" 
@@ -20,8 +21,13 @@ data = {'text':message,
 # sending post request and saving response as response object
 r = requests.post(url = API_ENDPOINT, data = data)
 status = str(r.status_code)
+parsed_json = json.loads(r.text)
+ok = str(parsed_json['ok'])
+if "error_code" in parsed_json:
+	error_code = str(parsed_json['error_code'])
+
 # extracting response text 
 if r.status_code == 200:
 	print ("Message sent!")
 else:
-	print "Error: " + r.text
+	print "OK: " + ok + "\n" + "Error: " + error_code + "\n" + "Description: " + parsed_json['description']

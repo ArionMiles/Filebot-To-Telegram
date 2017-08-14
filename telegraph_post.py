fddeef8d5a5a38179db7e63859ada378121fe2e6
Kanishk Singh <singh.kanishk07@gmail.com>
2017-08-15 00:17:58 +0530

    Added Telegraph submitter module
    Allows you to submit large amount of information to telegra.ph and forward it to telegram or print the link.

---
+++ telegraph_post.py
@@ -0,0 +1,29 @@
+import argparse
+from telegram import telegram
+from telegraph import Telegraph
+
+__author__ = 'Kanishk Singh (Arion Miles)'
+__license__ = "MIT"
+
+PARSER = argparse.ArgumentParser(description="Telegra.ph submitter")
+PARSER.add_argument('-title', '-t', type=str, help="Post title", required=False)
+#PARSER.add_argument('-content', '-c', type=str, nargs='*', help="Post content", required=False)
+ARGS = PARSER.parse_args()
+
+def telegraph_submit(message_title, message_content):
+    title = message_title
+    content = message_content
+    telegraph = Telegraph()
+    telegraph.create_account(short_name='FileBot')
+    response = telegraph.create_page(title, html_content=content)
+    posted = 'http://telegra.ph/{}'.format(response['path'])
+    telegram(title, posted)
+
+if __name__ == '__main__':
+    title = ARGS.title
+    content = ARGS.content
+    telegraph = Telegraph()
+    telegraph.create_account(short_name='FileBot')
+    response = telegraph.create_page(title, html_content=content)
+    posted = 'http://telegra.ph/{}'.format(response['path'])
+    print posted
